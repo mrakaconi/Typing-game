29387ddb022368d0807b4f124878baf1622f4448
Marko Jordanovic <mrakaconi@gmail.com>
2019-02-25 23:04:05 +0100

    1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // TODO: dodati stop
 const s = id => document.getElementById(id);
 const radioButtons = document.querySelectorAll('input[type="radio"]');
-const poljaSlova = document.querySelectorAll('.letters h2');
+const poljaSlova = document.querySelectorAll('key kbd');
 let numbers = [];
-let speed = 3500;
+let speed = 5000;
 let intervalId;
 let randomBroj;
 let taknuto = false;
@@ -13,7 +13,9 @@ let taknuto = false;
 function random() {
   const index = Math.floor(Math.random() * numbers.length);
   const izbaceno = numbers.splice(index, 1);
-  s('#number').innerText = randomBroj = izbaceno[0]; // precica
+  s('number').innerText = randomBroj = izbaceno[0]; // precica
+  console.log(izbaceno);
+  
   // s('number').innerText = izbaceno[0];
   // randomBroj = izbaceno[0];
 }
@@ -21,14 +23,14 @@ function random() {
 function gameLoop() {
   if (randomBroj && !taknuto) {
     poljaSlova[randomBroj-1].style.color = 'red';
-    s('#miss').innerText = Number(s('#miss').innerText) + 1;
+    s('miss').innerText = Number(s('miss').innerText) + 1;
   }
   if (numbers.length) {
     random();
-    s('#left').innerText = numbers.length;
+    s('left').innerText = numbers.length;
   } else {
     clearInterval(intervalId);
-    document.getElementById('#number').innerText = "Igra je završena";
+    document.getElementById('number').innerText = "Igra je završena";
   }
   taknuto = false;
 }
@@ -41,11 +43,11 @@ function init() {
   numbers = [];
   for (let i = 1; i <= 26; i++) {
     numbers.push(i);
-    poljaSlova[i-1].style.color = '#bdc5cc'; // setuje default boju
+    // poljaSlova[i-1].style.color = 'black'; // setuje default boju
   }
-  s('#hit').innerText = s('#miss').innerText = 0; // precica, videti gore
-  s('#left').innerText = numbers.length;
-  s('#number').innerText = "Pripremi se!";
+  s('hit').innerText = s('miss').innerText = 0; // precica, videti gore
+  s('left').innerText = numbers.length;
+  s('monitor').innerText = "Pripremi se!";
   clearInterval(intervalId);
   intervalId = setInterval(random, speed);
 }
@@ -58,10 +60,10 @@ function handleUserInput(e) {
 
   if (randomBroj + 64 === karakterBroj) {
     poljaSlova[randomBroj-1].style.color = 'green';
-    s('#hit').innerText = Number(s('#hit').innerText) + 1;
+    s('hit').innerText = Number(s('hit').innerText) + 1;
   } else {
     poljaSlova[randomBroj-1].style.color = 'red';
-    s('#miss').innerText = Number(s('#miss').innerText) + 1;
+    s('miss').innerText = Number(s('miss').innerText) + 1;
   }
 
   taknuto = true;
@@ -73,6 +75,6 @@ for (let i = 0; i < radioButtons.length; i++) {
   radioButtons[i].addEventListener('change', handleSpeedChange);
 }
 
-document.getElementById('#start').addEventListener('click', init);
+document.getElementById('start').addEventListener('click', init);
 
 window.addEventListener('keypress', handleUserInput);
